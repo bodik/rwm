5733fcca4d0932038035cfb77b0b90a4e0b52037
Radoslav Bodó <bodik@cesnet.cz>
2024-03-25 13:42:14 +0100

    rclone: drop args validation

--- rwm.py
+++ rwm.py
@@ -68,20 +68,8 @@ class RWM:
         # aws cli does not have endpoint-url as env config option
         return subrun(["aws", "--endpoint-url", self.config["S3_ENDPOINT_URL"]] + args, env=env, check=False).returncode
 
-    @staticmethod
-    def _rclone_validate_args(args):
-        """rclone args helper"""
-
-        if (not any(x.startswith("rwmbe:") for x in args)) and all(x != "help" for x in args):
-            logging.error("rclone command missing 'rwmbe:' backend specification")
-            return False
-        return True
-
     def rclone_cmd(self, args):
         """rclone wrapper"""
-
-        if not self._rclone_validate_args(args):
-            return 1
 
         env = {
             "RCLONE_CONFIG": "",
@@ -101,9 +89,6 @@ class RWM:
         * https://rclone.org/docs/#config-file
         * https://rclone.org/crypt/
         """
-
-        if not self._rclone_validate_args(args):
-            return 1
 
         env = {
             "RCLONE_CONFIG": "",
